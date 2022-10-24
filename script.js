282b80d5c38aab3e86efac6021d068c98221c91b
Sekoutouree <edwinsamm@gmail.com>
2022-10-24 22:14:01 +0000

    Added current score to else block

--- script.js
+++ script.js
@@ -39,11 +39,10 @@ btnRoll.addEventListener('click', function () {
   } else {
     // Switch to next player
     document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     //Change background on player switch
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
   }
 });
-
-//Generate random dice roll
