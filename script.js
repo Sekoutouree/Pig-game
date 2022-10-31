655701949efcec05ca59f9f53b41400361cb87cf
Sekoutouree <edwinsamm@gmail.com>
2022-10-31 14:30:55 +0000

    Added init function to reset game

--- script.js
+++ script.js
@@ -15,14 +15,26 @@ const btnHold = document.querySelector('.btn--hold');
 
 //STARTING CONDITIONS
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -69,7 +81,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 50) {
       // Finish game
       playing = false;
       diceEl.classList.add('hidden');
@@ -85,3 +97,21 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//RESETTING THE GAME
+
+btnNew.addEventListener('click', init);
+
+// btnNew.addEventListener('click', function () {
+//   playing = true;
+//   diceEl.classList.add('hidden');
+//   document;
+//   document.getElementById(`score--${activePlayer}`).textContent = 0;
+//   document.getElementById(`current--${activePlayer}`).textContent = 0;
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.remove('player--winner');
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.remove('player--active');
+// });
